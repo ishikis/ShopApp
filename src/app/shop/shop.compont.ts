85961d830b5bb8d3fa6ec94425fb57349fe73455
ishikis <ishik.is@hotmail.com>
2019-09-15 13:37:05 +0300

    Sayfalama

--- src/app/shop/shop.compont.ts
+++ src/app/shop/shop.compont.ts
@@ -13,14 +13,35 @@ import { Category } from '../model/category.model';
 
 })
 export class ShopComponent {
+    public selectedCategory: Category = null;
+    public productPerPage: number = 3;
+    public selectedPage: number = 1;
+
     constructor(private productRepository: ProductRepository
         , private categoryRepository: CategoryRepository) { }
 
-    public selectedCategory: Category = null;
 
     get products(): Product[] {
-        return this.productRepository.getProducts(this.selectedCategory)
+        let index = (this.selectedPage - 1) * this.productPerPage;
+        //sayfalama  için slice metodunu kullanıyoruz 
+        return this.productRepository
+            .getProducts(this.selectedCategory)
+            .slice(index, index + this.productPerPage);
     }
+
+    get pageNumbers(): number[] {
+        return Array(
+            Math.ceil(this.productRepository
+            .getProducts(this.selectedCategory).length / this.productPerPage))
+            .fill(0)
+            .map((a, i) => i + 1)
+    }
+
+    changePage(p: number) {
+        this.selectedPage = p;
+    }
+
+
     get categories(): Category[] {
         return this.categoryRepository.getCategories()
     }
