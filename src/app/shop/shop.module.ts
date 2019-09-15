9385304024500c3662b597e122900c15567030c0
ishikis <ishik.is@hotmail.com>
2019-09-15 11:49:06 +0300

    navbar

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,11 +3,12 @@ import { ModelModule } from '../model/model.module';
 import { ShopComponent } from './shop.compont';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [ShopComponent],
+    declarations: [ShopComponent, NavbarComponent],
     exports: [ShopComponent]
 })
 
