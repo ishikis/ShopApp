f54870a7cc37c94ca1527b0b8a2f6f125c402033
ishikis <ishik.is@hotmail.com>
2019-09-15 11:10:30 +0300

    repository işlemleri

---
+++ src/app/model/category.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnInit } from '@angular/core'
+import { RestService } from './rest.service';
+import { Category } from './category.model';
+
+
+@Injectable()
+
+export class CategoryRepository implements OnInit {
+
+    private categories: Category[] = [];
+    constructor(private restService: RestService) { }
+
+    ngOnInit() {
+
+        this.restService.getCategories().
+            subscribe(categories => this.categories = categories);
+    }
+
+    getCategory(id: number): Category {
+        return this.categories.find(i => i.id === id);
+    }
+
+}
---
+++ src/app/model/model.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RestService } from './rest.service';
+import { ProductRepository } from './product.repository';
+import { CategoryRepository } from './category.repository';
+
+
+@NgModule({
+    imports: [HttpClientModule],
+    providers: [RestService, ProductRepository, CategoryRepository]
+})
+
+
+export class ModelModule {
+
+}
