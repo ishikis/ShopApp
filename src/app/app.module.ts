030909c7b15f267cd493a682081251f1f8fca190
ishikis <ishik.is@hotmail.com>
2019-09-15 15:59:00 +0300

    root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShopModule } from './shop/shop.module';
+import { RouterModule } from "@angular/router";
+import { ShopComponent } from './shop/shop.compont';
+import { CartDetailComponent } from './shop/cart-detail/cart-detail.component';
+import { CheckoutComponent } from './shop/checkout/checkout.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { ShopModule } from './shop/shop.module';
   ],
   imports: [
     BrowserModule,
-    ShopModule
+    ShopModule,
+    RouterModule.forRoot([
+      { path: 'shop', component: ShopComponent },
+      { path: 'card', component: CartDetailComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: '**', redirectTo: "/shop" } 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [ShopComponent, NavbarComponent, CartSummaryComponent],
-    exports: [ShopComponent]
+    declarations: [ShopComponent, NavbarComponent, CartSummaryComponent, CartDetailComponent, CheckoutComponent],
+    exports: [ShopComponent, CartDetailComponent, CheckoutComponent]
 })
 
 
