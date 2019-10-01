e810cf2e686b85e95ab5a04c0299cca25ce54555
ishikis <ishik.is@hotmail.com>
2019-10-01 19:00:54 +0300

    AutGuard Yetki işlemleri.

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
-  { path: 'main', component: AdminComponent },
+  //yetkisiz girişleri kontrol  etmek için canActivate Ekledik AuthGuard içinde ise koşullarımız var
+  { path: 'main', component: AdminComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'auth' }
 ];
 
