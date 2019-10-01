bdfa50346f88a5baad67785047342ad5ad09cc5b
ishikis <ishik.is@hotmail.com>
2019-10-01 23:00:23 +0300

    admin sayfaları eklendi

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,10 +6,15 @@ import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin.component';
 import { FormsModule } from '@angular/forms';
 import { AuthGuard } from './auth.guard';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+import { CategoryListComponent } from './categories/category-list/category-list.component';
+import { OrderListComponent } from './orders/order-list/order-list.component';
+import { CategoryFormComponent } from './categories/category-form/category-form.component';
 
 
 @NgModule({
-  declarations: [AuthComponent, AdminComponent],
+  declarations: [AuthComponent, AdminComponent, ProductListComponent, ProductFormComponent, CategoryFormComponent, CategoryListComponent, OrderListComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
